9f3a21374e1c075bfbca414d90700a593decc1a6
Nat Grauman <nat@metarhythm.com>
2011-11-05 19:52:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nov5tutorial::Application.config.secret_token = '405e534c73e0e06c480b02570d3a6c942174a77a7c54a2351d796451efce7b431f143e47aa3c5bc888850c51e399b1e6ad2d3d5c68b0c7f6a7c3bad886aa66a6'
